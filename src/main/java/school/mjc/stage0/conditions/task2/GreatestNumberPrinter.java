b1dd8ec81b86b15bf50ffd5b9b51ff5234b323af
ArshakyanGrigor <grigor.arshakyan1994@mail.ru>
2022-12-15 11:39:07 +0400

    Updated

---
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -0,0 +1,13 @@
+package school.mjc.stage0.conditions.task2;
+
+public class GreatestNumberPrinter {
+    public void printGreatest(int first, int second) {
+        if (first > second) {
+            System.out.println(first);
+        } else if (first == second) {
+            System.out.println(first + "\n" + second);
+        } else {
+            System.out.println(second);
+        }
+    }
+}
