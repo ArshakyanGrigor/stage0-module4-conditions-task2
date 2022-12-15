4363cb04cb69417cf973cde56f306a61e6e6c328
ArshakyanGrigor <grigor.arshakyan1994@mail.ru>
2022-12-15 11:32:02 +0400

    Updated file

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++
@@ -1,7 +0,0 @@
-package school.mjc.stage0.conditions.task2;
-
-public class CoinFlip {
-    public void throwCoin(int from1UpTo1000) {
-
-    }
-}
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++
@@ -1,6 +0,0 @@
-package school.mjc.stage0.conditions.task2;
-
-public class GreatestNumberPrinter {
-    public void printGreatest(int first, int second) {
-    }
-}
